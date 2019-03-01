6e0a52ef1dc285bad28c143da09f235ac506274b
Ryan <ryanstunkel@blipsounds.com>
2019-03-01 10:54:27 -0500

    FMOD Integration Successful

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FMODUnity.RuntimeManager.PlayOneShot("event:/TestSound");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
