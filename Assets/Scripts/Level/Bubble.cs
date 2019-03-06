cec75bbfbacf81396ab765c812c05b7fd6cbf7ec
Ryan <ryanstunkel@blipsounds.com>
2019-03-06 15:49:36 -0500

    Created new FMOD events and Added Debug.Log to Bubble.cs for testing
    Added a sound access test

--- Assets/Scripts/Level/Bubble.cs
+++ Assets/Scripts/Level/Bubble.cs
@@ -920,6 +920,7 @@ public class Bubble : MonoBehaviour {
         _velocity = new Vector2(speed * dir.x, speed * dir.y);
         GetComponent<CircleCollider2D>().enabled = true;
         wasThrown = true;
+        Debug.Log("Thrown");
     }
 
     public void HideSprites() {
