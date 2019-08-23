895739b0eadd85b9254928f72d01533e1a980f95
BMO\Kevin <kmccorkle9001@gmail.com>
2019-08-23 16:27:47 +0900

    Updating Beach Sea Creatures
    Added some extra animations to the beach sea creatures and refactored the code files a bit.

---
+++ Assets/Scripts/Level/Beach/BobbingSeaCreature.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobbingSeaCreature : SeaCreature {
+
+    // Moves in a wavy motion
+    float _waveSpeed = 0f;
+    bool _waveUp = true;
+
+    protected new void Awake() {
+        base.Awake();
+    }
+    // Use this for initialization
+    protected new void Start() {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (_waveUp) {
+            _waveSpeed += Time.deltaTime;
+            if (_waveSpeed >= 0.75f) {
+                _waveUp = false;
+            }
+        } else {
+            _waveSpeed -= Time.deltaTime;
+            if (_waveSpeed <= -0.75f) {
+                _waveUp = true;
+            }
+        }
+
+        transform.Translate(moveSpeed * Time.deltaTime, _waveSpeed * Time.deltaTime, 0f);
+    }
+}
---
+++ Assets/Scripts/Level/Beach/StraightSeaCreature.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightSeaCreature : SeaCreature {
+
+    protected new void Awake() {
+        base.Awake();
+    }
+    // Use this for initialization
+    protected new void Start() {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(moveSpeed * Time.deltaTime, 0f, 0f);
+    }
+}
